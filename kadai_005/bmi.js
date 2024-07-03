b8e21fc4305fa10f8cb514cb241c45618c796839
0912seiren <164618006+0912seiren@users.noreply.github.com>
2024-07-03 22:06:41 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,19 @@
+let weight;
+
+// 値の代入
+weight = 68;
+
+// 変数の宣言・値の代入
+let height;
+
+height = 1.7;
+
+// コンソールへの出力
+console.log(weight);
+console.log(height);
+
+let bmi;
+
+bmi = ( 68 / (1.7 * 1.7) );
+ 
+console.log(bmi);
